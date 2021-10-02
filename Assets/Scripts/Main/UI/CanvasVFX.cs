11e3b69432ee91dc9a2d76757569d97d2661ed0f
Djeye <nick7@inbox.ru>
2021-10-03 02:12:57 +0300

    [add] add vfx, animations, restart system / complete game

---
+++ Assets/Scripts/Main/UI/CanvasVFX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasVFX : MonoBehaviour
+{
+    [SerializeField] Image star;
+    [SerializeField] Image heart;
+
+    private void Start()
+    {
+        Core.addScoreEvent += SpawnStar;
+        Core.removeLiveEvent += SpawnHeart;
+    }
+
+    private void SpawnStar()
+    {
+        Instantiate(star, transform);
+    }
+
+    private void SpawnHeart()
+    {
+        Instantiate(heart, transform);
+    }
+
+    private void OnDisable()
+    {
+        Core.addScoreEvent -= SpawnStar;
+        Core.removeLiveEvent -= SpawnHeart;
+    }
+}
---
+++ Assets/Scripts/Main/VFX/SingleUsable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleUsable : Effect
+{
+    public override void EndAnimation()
+    {
+        Destroy(this.gameObject);
+    }
+}
